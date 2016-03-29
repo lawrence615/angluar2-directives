f3b0e9ca147b83daad0c5f392649dd79724e6707
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-29 18:36:10 +0300

    defined an inputs array with variable highlightColor and alias myHighlight
    changed
    
    this._elRef.nativeElement.style.backgroundColor = this._defaultColour;
    
    to
    
    this._renderer.setElementStyle(this._elRef.nativeElement,
    'background-color', this.highlightColor || this._defaultColor);

--- dev/highlight.directive.ts
+++ dev/highlight.directive.ts
@@ -1,22 +1,23 @@
 import {Directive} from 'angular2/core';
 import {ElementRef} from "angular2/core";
 import {OnInit} from "angular2/core";
-/**
- * Created by Lawrence on 3/22/16.
- */
+import {Renderer} from "angular2/core";
 
 @Directive({
     selector: '[myHighlight]',
+    inputs: ['highlightColor:myHighlight']
 })
 
 export class HighlightDirective implements OnInit {
-    private _defaultColour: 'green';
+    private _defaultColor = 'green';
+    highlightColor:string;
 
-    constructor(private _elRef: ElementRef) {}
+    constructor(private _elRef:ElementRef, private _renderer:Renderer) {
+    }
 
 
     ngOnInit():any {
-        this._elRef.nativeElement.style.backgroundColor = this._defaultColour;
+        this._renderer.setElementStyle(this._elRef.nativeElement, 'background-color', this.highlightColor || this._defaultColor);
     }
 
 }
