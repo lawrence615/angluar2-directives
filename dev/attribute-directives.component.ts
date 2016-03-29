c1a9e9bca632309206d99dce64eeceed5c0a744d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-29 18:34:12 +0300

    passing a value to the directive
    renamed AttributeDirectivesComponent to AttributeDirectives

--- dev/attribute-directives.component.ts
+++ dev/attribute-directives.component.ts
@@ -4,13 +4,16 @@ import {HighlightDirective} from "./highlight.directive";
 @Component({
     selector:'my-attribute-directives',
     template:`
-        <h5 myHighlight>
+        <h5 [myHighlight]="'red'">
              Highlight me
         </h5>
+        <div [myHighlight]="'yellow'">
+            Highlight me Also
+        </div>
     `,
     directives:[HighlightDirective]
 })
 
-export class AttributeDirectivesComponent{
+export class AttributeDirectives{
 
 }
