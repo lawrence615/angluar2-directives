6906bfb07226462ce06c7bc220a538df27938cb8
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-29 20:13:58 +0300

    started with *ngIf structural directive
    We are checking if a number inputed is greater than 10, if so display
    some text

--- dev/structural-directives.component.ts
+++ dev/structural-directives.component.ts
@@ -1,11 +1,22 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector:'my-structural-directives',
-    template:`
+    selector: 'my-structural-directives',
+    template: `
+        <section class="directive">
+            <h2>*ngif</h2>
+            <div>
+                Enter a number higher than 10
+                <br>
+                <input type="input" #number (keyup)="0">
+            </div>
+            <div *ngIf=" number.value > 10">
+                Number is greater than 10
+            </div>
+        </section>
     `
 })
 
-export class StructuralDirectives{
+export class StructuralDirectives {
 
 }
